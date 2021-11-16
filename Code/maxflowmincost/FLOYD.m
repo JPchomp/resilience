26ebb1abc987172a79c73fb214f1d2b9c3cfd524
JPchomp <52250025+JPchomp@users.noreply.github.com>
2021-11-16 09:54:13 +0100

    updated

--- Code/maxflowmincost/FLOYD.m
+++ Code/maxflowmincost/FLOYD.m
@@ -1,8 +1,11 @@
 function [L,R]=FLOYD(w,s,t)
+
 n=size(w,1);
 D=w;
 Path=zeros(n,n);
+
 % below is the standard floyd algorithm
+
 for i=1:n
  for j=1:n
  if D(i,j)~=inf
@@ -10,6 +13,7 @@ for i=1:n
  end
  end
 end
+
 for k=1:n
  for i=1:n
  for j=1:n
@@ -20,15 +24,19 @@ for k=1:n
  end
  end
 end
+
 L=zeros(0,0);
 R=s;
+
 while 1
  if s==t
  L=fliplr(L);
  L=[0,L];
  return
  end
+
  L=[L,D(s,t)];
  R=[R,Path(s,t)];
  s=Path(s,t);
+
 end
